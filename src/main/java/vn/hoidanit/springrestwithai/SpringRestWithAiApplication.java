061318698ffa1e79db28e747fe5cad4b7326e210
Tiep <lenhotiep1992004@gmail.com>
2026-05-07 16:55:49 +0700

    init project

---
+++ src/main/java/vn/hoidanit/springrestwithai/SpringRestWithAiApplication.java
@@ -0,0 +1,13 @@
+package vn.hoidanit.springrestwithai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWithAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hoidanit/hoidanit_spring_rest_with_ai/HoidanitSpringRestWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hoidanit.hoidanit_spring_rest_with_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoidanitSpringRestWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
